1b3956b98491732be0fc644bf67a36a4975150d8
Alex Goodwin <hagoodwi@gmail.com>
2020-12-18 11:16:42 -0800

    add all 50 states to chart

--- app.rb
+++ app.rb
@@ -6,17 +6,60 @@ also_reload('lib/**/*.rb')
 
 
 get('/') do
-  covid = Covid.new(' ')
+  covid = Covid.new(' ga')
   @al = covid.al
   @ak = covid.ak
   @az = covid.az
-
-
-
-
+  @ar = covid.ar
+  @ca = covid.ca
+  @co = covid.co
+  @ct = covid.ct
+  @de = covid.de
   @ga = covid.ga
+  @hi = covid.hi
+  @id = covid.id
+  @il = covid.il
+  @in = covid.in
+  @ia = covid.ia
+  @ks = covid.ks
+  @ky = covid.ky
+  @la = covid.la
+  @me = covid.me
+  @md = covid.md
+  @ma = covid.ma
+  @mi = covid.mi
+  @mn = covid.mn
+  @ms = covid.ms
+  @mo = covid.mo
+  @mt = covid.mt
+  @ne = covid.ne
+  @nv = covid.nv
+  @nh = covid.nh
+  @nj = covid.nj
+  @nm = covid.nm 
+  @ny = covid.ny
+  @nc = covid.nc
+  @nd = covid.nd
+  @oh = covid.oh
+  @ok = covid.ok
+  @or = covid.or
+  @pa = covid.pa
+  @ri = covid.ri
+  @sc = covid.sc
+  @sd = covid.sd
+  @tn = covid.tn  
+  @tx = covid.tx
+  @ut = covid.ut
+  @vt = covid.vt
+  @va = covid.va  
+  @wa = covid.wa
+  @wv = covid.wv
+  @wi = covid.wi 
+  @wy= covid.wy
+
+
+
   @usCases = covid.get_us
-  @state = covid.get_state
   @positives = covid.get_positives
   @update = covid.update
   @usDeaths = covid.us_death
--- lib/covid.rb
+++ lib/covid.rb
@@ -20,25 +20,193 @@ class Covid
     response = HTTParty.get('https://api.covidtracking.com/v1/states/az/current.json')
     response ['deathIncrease'] 
   end
-
-
-
-
-
-
-
-
-
-
-
-
+  def ar
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/ar/current.json')
+    response ['deathIncrease'] 
+  end
+  def ca
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/ca/current.json')
+    response ['deathIncrease'] 
+  end
+  def co
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/co/current.json')
+    response ['deathIncrease'] 
+  end
+  def ct
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/ct/current.json')
+    response ['deathIncrease'] 
+  end
+  def de
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/de/current.json')
+    response ['deathIncrease'] 
+  end
+  def fl
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/fl/current.json')
+    response ['deathIncrease'] 
+  end
   def ga
     response = HTTParty.get('https://api.covidtracking.com/v1/states/ga/current.json')
     response ['deathIncrease'] 
   end
-
-  def get_state
-    return @state
+  def hi
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/hi/current.json')
+    response ['deathIncrease'] 
+  end
+  def id
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/id/current.json')
+    response ['deathIncrease'] 
+  end
+  def il
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/il/current.json')
+    response ['deathIncrease'] 
+  end
+  def in
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/in/current.json')
+    response ['deathIncrease'] 
+  end
+  def ia
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/ia/current.json')
+    response ['deathIncrease'] 
+  end
+  def ks
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/ks/current.json')
+    response ['deathIncrease'] 
+  end
+  def ky
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/ky/current.json')
+    response ['deathIncrease'] 
+  end
+  def la
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/la/current.json')
+    response ['deathIncrease'] 
+  end
+  def me
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/me/current.json')
+    response ['deathIncrease'] 
+  end
+  def md
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/md/current.json')
+    response ['deathIncrease'] 
+  end
+  def ma
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/ma/current.json')
+    response ['deathIncrease'] 
+  end
+  def mi
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/mi/current.json')
+    response ['deathIncrease'] 
+  end
+  def mn
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/mn/current.json')
+    response ['deathIncrease'] 
+  end
+  def ms
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/ms/current.json')
+    response ['deathIncrease'] 
+  end
+  def mo
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/mo/current.json')
+    response ['deathIncrease'] 
+  end
+  def mt
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/mt/current.json')
+    response ['deathIncrease'] 
+  end
+  def ne
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/ne/current.json')
+    response ['deathIncrease'] 
+  end
+  def nv
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/nv/current.json')
+    response ['deathIncrease'] 
+  end
+  def nh
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/nh/current.json')
+    response ['deathIncrease'] 
+  end
+  def nj
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/nj/current.json')
+    response ['deathIncrease'] 
+  end
+  def nm
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/nm/current.json')
+    response ['deathIncrease'] 
+  end
+  def ny
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/ny/current.json')
+    response ['deathIncrease'] 
+  end
+  def nc
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/nc/current.json')
+    response ['deathIncrease'] 
+  end
+  def nd
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/nd/current.json')
+    response ['deathIncrease'] 
+  end
+  def oh
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/oh/current.json')
+    response ['deathIncrease'] 
+  end
+  def ok
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/ok/current.json')
+    response ['deathIncrease'] 
+  end
+  def or
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/or/current.json')
+    response ['deathIncrease'] 
+  end
+  def pa
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/pa/current.json')
+    response ['deathIncrease'] 
+  end
+  def ri
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/ri/current.json')
+    response ['deathIncrease'] 
+  end
+  def sc
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/sc/current.json')
+    response ['deathIncrease'] 
+  end
+  def sd
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/sd/current.json')
+    response ['deathIncrease'] 
+  end
+  def tn
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/tn/current.json')
+    response ['deathIncrease'] 
+  end
+  def tx
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/tx/current.json')
+    response ['deathIncrease'] 
+  end
+  def ut
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/ut/current.json')
+    response ['deathIncrease'] 
+  end
+  def vt
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/vt/current.json')
+    response ['deathIncrease'] 
+  end
+  def va
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/va/current.json')
+    response ['deathIncrease'] 
+  end
+  def wa
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/wa/current.json')
+    response ['deathIncrease'] 
+  end
+  def wv
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/wv/current.json')
+    response ['deathIncrease'] 
+  end
+  def wi
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/wi/current.json')
+    response ['deathIncrease'] 
+  end
+  def wy
+    response = HTTParty.get('https://api.covidtracking.com/v1/states/wy/current.json')
+    response ['deathIncrease'] 
   end
   def get_positives
     response = HTTParty.get('https://api.covidtracking.com/v1/states/' + @state +  '/current.json')
@@ -57,5 +225,7 @@ class Covid
     response = HTTParty.get('https://api.covidtracking.com/v1/us/current.json')
     response [0]['deathIncrease'] 
   end
+
 end
+
 
